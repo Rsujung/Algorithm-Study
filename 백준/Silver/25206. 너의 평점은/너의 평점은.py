fe16a5eb441677a87e351d2ecb05b7d14d548e25
Rsujung <141989419+Rsujung@users.noreply.github.com>
2023-11-02 16:10:57 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,19 @@
+dic = {'A+':4.5, 'A0':4.0, 'B+':3.5, 'B0':3.0, 
+       'C+':2.5, 'C0':2.0, 'D+':1.5, 'D0':1.0,'F':0.0 ,'P':None}
+
+T = 20
+score_1 = []
+score_2 = []
+
+for i in range(T):
+    lst = input().split(' ')
+    if lst[2] == 'P':
+        pass
+    else:
+        hack = float(lst[1]) * float(dic[lst[2]])
+        hap = float(lst[1])
+        score_1.append(hack)
+        score_2.append(hap)
+
+f_score = sum(score_1) / sum(score_2)
+print(round(f_score,6))
