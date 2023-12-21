b69afc659d15016131e22765e25fdbf196281467
Rsujung <141989419+Rsujung@users.noreply.github.com>
2023-12-21 12:51:30 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT animal_ins.name, animal_ins.datetime
+from animal_ins left join animal_outs on animal_ins.animal_id = animal_outs.animal_id
+where animal_outs.datetime is null
+order by animal_ins.datetime
+limit 3;
